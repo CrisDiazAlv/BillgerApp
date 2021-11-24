f8b9a1e610fd759d8d9138724545a545823fbd1e
Cris <crisdiazalv@gmail.com>
2021-11-24 19:25:55 +0100

    Add window constraints

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import MainScreen from "./screens/MainScreen";
+import { Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  return <MainScreen />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <MainScreen />
+        
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
